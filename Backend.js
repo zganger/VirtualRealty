91b688f0643840d671b888de2ad8a890c5ad1ad5
Zack Ganger <zganger@bu.edu>
2015-10-27 19:25:35 -0400

    add backend constructors-- incomplete

---
+++ Backend.js
@@ -0,0 +1,78 @@
+function Piece(Owner, type)
+{
+    this.owner = Owner;
+    this.type = type;
+    this.position =
+}
+
+function Board()
+{
+    this.tiles = new Array();
+    var spaces = ["Go", "Meditteranean Avenue", ""] //add all tiles
+    var values = [0, 60, 0, 60] //all values correspond with title index
+    var mortgages = [] //mortgage values here
+    //matrix of rents
+    //colorgroups (null if not property)
+    for(var i = 0; i < 40; i++)
+    {
+        var newTile = Tile(spaces[i], values[i]); //more interitance
+        tiles.push(newTile);
+    }
+    this.pieces = new Array();
+    pieces.push("racecar");
+    pieces.push("tophat");
+}
+
+function Owner()
+{
+    this.Proerties
+    this.money = 1500;
+    this.Assets
+    this.Color
+}
+
+function Tile(title, value, rents, mortgage, colorGroup)
+{
+    this.title = title;
+    if(colorGroup != null)
+    {
+        this.property = Property(value, rents, mortgage, colorGroup);
+    }
+    else
+    {
+        if(title == "Go")
+        {
+            this.action=
+        }
+        else if(title == "Chance")
+        {
+            //random number
+            this.action = Chance[rand]; //not in constructor
+        }
+        //for all possible action tiles
+    }
+}
+
+function Property(value, rents, mortgage, colorGroup)
+{
+    this.mortgage = mortgage;
+    this.isMortgaged = false;
+    this.value = value;
+    this.buildings = 0;
+    this.rents = rents;
+    this.colorGroup = colorGroup;
+    this.player = null;
+}
+
+function Card(type, text, m, value)
+{
+    this.type = type;
+    this.text = text;
+    this.command = new Action(m, value);
+}
+
+function Action(m, value)
+{
+    this.m = m;
+    this.value = value;
+}

2a46266298789e7c0bb5d76ccb50fc52c6d38e63
nmemme <nmemme@bu.edu>
2015-10-27 21:28:41 -0400

    Robot function prototypes
    Initial commit

---
+++ robot.h
@@ -0,0 +1,17 @@
+
+void initSensors();
+void initBluetooth();
+
+string getBluetooth();
+string decodeBluetooth(string);
+
+void startMotors(int);
+bool checkDestination();
+? pollLineFollowing(?);
+bool LineDetected();
+
+bool objectDetected();
+
+void stopMotors();
+
+void sendBluetooth();

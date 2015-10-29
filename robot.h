ec7330677c661f048568681c6bd48f03eba50a83
nmemme <nmemme@bu.edu>
2015-10-29 18:26:32 -0400

    update function prototypes

--- robot.h
+++ robot.h
@@ -1,17 +1,18 @@
 
 void initSensors();
 void initBluetooth();
+void initVariables();
 
 string getBluetooth();
 string decodeBluetooth(string);
 
 void startMotors(int);
 bool checkDestination();
-? pollLineFollowing(?);
+int pollLineFollowing();
 bool LineDetected();
 
 bool objectDetected();
 
 void stopMotors();
 
-void sendBluetooth();
+void sendBluetooth(string);

ffc83113247585a66e8be97ccc6a3da7c192df18
Zach Sarkis <zacherysarkis@gmail.com>
2016-03-14 12:58:21 -0400

    updates location
    changes location of pieces in movep2

--- Monopoly/Assets/_Scrips/MoveP2.cs
+++ Monopoly/Assets/_Scrips/MoveP2.cs
@@ -14,13 +14,8 @@ public class MoveP2 : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-
-	}
-
-	public void MoveTo (int inDest, Board gameBoard)
-	{
 		bool isFinished = false;
-		while (isFinished==false && (transform.position.x != gameBoard.Unitycoordinates [inDest, 0] || transform.position.x != gameBoard.Unitycoordinates [inDest, 1])) {
+		while (isFinished==false && (transform.position.x != GamePlay.GameBoard.Unitycoordinates [Dest, 0])) { //|| transform.position.x != gameBoard.Unitycoordinates [inDest, 1])) {
 			if (transform.position.x > 240 && transform.position.y < 42) { //will skip the tiniest bit
 				transform.position = new Vector3 (transform.position.x - 60,40,0);// * Time.deltaTime, 40, 0);
 			} else if (transform.position.x <= 240 && transform.position.y <= 350) {
@@ -31,7 +26,11 @@ public class MoveP2 : MonoBehaviour {
 				transform.position = new Vector3 (550, transform.position.y - 60,0);// * Time.deltaTime, 0);	//this is the right
 			}
 			isFinished = true;
-			Debug.Log(transform.position);
 		}
+	}
+
+	public void MoveTo (int inDest, Board gameBoard)
+	{
+		Dest = inDest;
 	}
 }

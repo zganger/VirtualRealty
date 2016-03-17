67dd8fd0c179ada3a52fccefdfb1d667a47c2e34
Zach Sarkis <zacherysarkis@gmail.com>
2016-03-17 18:38:36 -0400

    ALMOST got movement down i think

--- Monopoly/Assets/Scripts/UnityPieceImage.cs
+++ Monopoly/Assets/Scripts/UnityPieceImage.cs
@@ -21,7 +21,7 @@ public class UnityPieceImage : MonoBehaviour {
 	{
 		movementFinished = false;
 		while (!movementFinished) {
-			if ((transform.position.x + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 0) || transform.position.x - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 0)) && (transform.position.y + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 1) || transform.position.y - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 1))) {
+			if ((transform.position.x + 200 >= GamePlay.GameBoard.getUnityCoords (Dest, 0) || transform.position.x - 200 <= GamePlay.GameBoard.getUnityCoords (Dest, 0)) && (transform.position.y + 200 >= GamePlay.GameBoard.getUnityCoords (Dest, 1) || transform.position.y - 200 <= GamePlay.GameBoard.getUnityCoords (Dest, 1))) {
 				movementFinished = true;
 			} else if (transform.position.x > 240 && transform.position.y < 41) { //will skip the tiniest bit
 				transform.position = new Vector3 (transform.position.x - 60 * Time.deltaTime, 40, 0);
@@ -39,24 +39,49 @@ public class UnityPieceImage : MonoBehaviour {
 	void Update ()
 	{
 		movementFinished = true;
-		if (!(transform.position.x + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 1))||(transform.position.x - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 1))||(transform.position.x + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 1))||(transform.position.x - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 1)))
+		//check if coordinates are in range of the specified location
+		if (!(transform.position.x + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 1)))
+		{ movementFinished = false; } 
+		else if (!(transform.position.x - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 1)))
+		{ movementFinished = false; } 
+		else if(!(transform.position.x + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 1)))
+		{ movementFinished = false; } 
+		else if(!(transform.position.x - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 1)))
+		{ movementFinished = false; }
+		while (!movementFinished)
 		{
-			movementFinished = false;
-		}
-		while (movementFinished== false && (transform.position.x != GamePlay.GameBoard.getUnityCoords(Dest, 0) || transform.position.y != GamePlay.GameBoard.getUnityCoords(Dest, 1))) {
-			if (transform.position.x > 240 && transform.position.y < 41) { //will skip the tiniest bit
-				transform.position = new Vector3 (transform.position.x - 60 * Time.deltaTime, 40, 0);
-			} else if (transform.position.x <= 240 && transform.position.y <= 350) {
-				transform.position = new Vector3 (240, transform.position.y + 60 * Time.deltaTime, 0);	//this is the left
-			} else if (transform.position.x <= 550 && transform.position.y >= 350) {
-				transform.position = new Vector3 (transform.position.x + 60 * Time.deltaTime, 350, 0);	//this is top
-			} else {
-				transform.position = new Vector3 (550, transform.position.y - 60 * Time.deltaTime, 0);	//this is the right
+			if (transform.position.x >= 150 && transform.position.y <= -150) { //THESE NEED A BUFFER AND IT SHOULD WORK!
+				moveLeft();
+			} else if (transform.position.x <= -150 && transform.position.y <= -150) {
+				moveUp();
+			} else if (transform.position.x <= -150 && transform.position.y >= 150) {
+				moveRight();
+			} else if(transform.position.x >= 150 && transform.position.y >= 150){
+				moveDown();
 			}
 			movementFinished=true;
 		}
 	}
-
+	public void moveLeft ()
+	{
+		Debug.Log("moving Left");
+		transform.position = new Vector3 (transform.position.x - 60 * Time.deltaTime, -150, 0);	//this is bottom
+	}
+	public void moveUp ()
+	{
+		Debug.Log("moving Up");
+		transform.position = new Vector3 (-150, transform.position.y + 60 * Time.deltaTime, 0);	//this is the left
+	}
+	public void moveRight ()
+	{
+		Debug.Log("moving Right");
+		transform.position = new Vector3 (transform.position.x + 60 * Time.deltaTime, 150, 0);
+	}
+	public void moveDown ()
+	{
+		Debug.Log("moving Down");
+		transform.position = new Vector3 (150, transform.position.y - 60 * Time.deltaTime, 0);	//this is the right
+	}
 	public void MoveTo (int inDest)
 	{
 		Dest = inDest;

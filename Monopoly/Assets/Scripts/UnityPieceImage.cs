ee1967f0674774795a83c7bbabd78f7f2222ff15
Zach Sarkis <zacherysarkis@gmail.com>
2016-03-21 11:03:44 -0400

    pieces now jump to locations specified

--- Monopoly/Assets/Scripts/UnityPieceImage.cs
+++ Monopoly/Assets/Scripts/UnityPieceImage.cs
@@ -40,68 +40,70 @@ public class UnityPieceImage : MonoBehaviour {
 	public float xLenient, yLenient;
 	void Update ()
 	{
-		movementFinished = true;
 		//check if coordinates are in range of the specified location
 		if (!(transform.position.x + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 1))) {
-//			Debug.Log("hello1");
 			xLenient = transform.position.x-offX;	//ACCOUNT FOR DIFFERENT ORIGINS
 			yLenient = transform.position.y-offY;	//ACCOUNT FOR DIFFERENT ORIGINS
 			movementFinished = false;
 		} else if (!(transform.position.x - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 1))) { 	
-			Debug.Log("hello2");
 			xLenient = transform.position.x-offX;	//ACCOUNT FOR DIFFERENT ORIGINS
 			yLenient = transform.position.y-offY;	//ACCOUNT FOR DIFFERENT ORIGINS
 			movementFinished = false;
 		} else if(!(transform.position.x + 5 >= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 1))) {
-//			Debug.Log("hello3");
 			xLenient = transform.position.x-offX;	//ACCOUNT FOR DIFFERENT ORIGINS
 			yLenient = transform.position.y-offY;	//ACCOUNT FOR DIFFERENT ORIGINS
 			movementFinished = false;
 		} else if(!(transform.position.x - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 0) && transform.position.y - 5 <= GamePlay.GameBoard.getUnityCoords (Dest, 1))) {
-//			Debug.Log("hello4");
 			xLenient = transform.position.x-offX;	//ACCOUNT FOR DIFFERENT ORIGINS
 			yLenient = transform.position.y-offY;	//ACCOUNT FOR DIFFERENT ORIGINS
 			movementFinished = false;
 		}
 		while (!movementFinished)
 		{
+//			Debug.Log("Dest: " + GamePlay.GameBoard.getUnityCoords (Dest, 0) + ", " + GamePlay.GameBoard.getUnityCoords (Dest, 1));
 //			Debug.Log(xLenient + "," + yLenient);
-			if (xLenient >= -148 && yLenient <= -148) { //THESE NEED A BUFFER AND IT SHOULD WORK!
-				moveLeft();
+			if (xLenient >= -148 && yLenient <= -148) {
+//				moveLeft();
+				Debug.Log("moving left");
 			} else if (xLenient <= -148 && yLenient <= 148) {
-				moveUp();
+//				moveUp();
+				Debug.Log("moving up");
 			} else if (xLenient <= 148  && yLenient >= 148 ) {//(xLenient <= -150  &&)
-				moveRight();
+//				moveRight();
+				Debug.Log("moving right");
 			} else {
-				moveDown();
+//				moveDown();
+				
+				Debug.Log("moving down");
 			}
 			movementFinished=true;
-			Debug.Log("STOP MOVING");
+//			Debug.Log("tru");
 		}
 	}
 
-	public void moveLeft ()
-	{
-//		Debug.Log("moving Left");
-		transform.position = new Vector3 (transform.position.x - 60 * Time.deltaTime, transform.position.y, 0);	//this is bottom
-	}
-	public void moveUp ()
-	{
-//		Debug.Log("moving Up");
-		transform.position = new Vector3 (transform.position.x, transform.position.y + 60 * Time.deltaTime, 0);	//this is the left
-	}
-	public void moveRight ()
-	{
-//		Debug.Log("moving Right");
-		transform.position = new Vector3 (transform.position.x + 60 * Time.deltaTime, transform.position.y, 0);
-	}
-	public void moveDown ()
-	{
-//		Debug.Log("moving Down");
-		transform.position = new Vector3 (transform.position.x, transform.position.y - 60 * Time.deltaTime, 0);	//this is the right
-	}
+//	public void moveLeft ()
+//	{
+////		Debug.Log("moving Left");
+//		transform.position = new Vector3 (transform.position.x - 60 * Time.deltaTime, transform.position.y, 0);	//this is bottom
+//	}
+//	public void moveUp ()
+//	{
+////		Debug.Log("moving Up");
+//		transform.position = new Vector3 (transform.position.x, transform.position.y + 60 * Time.deltaTime, 0);	//this is the left
+//	}
+//	public void moveRight ()
+//	{
+////		Debug.Log("moving Right");
+//		transform.position = new Vector3 (transform.position.x + 60 * Time.deltaTime, transform.position.y, 0);
+//	}
+//	public void moveDown ()
+//	{
+////		Debug.Log("moving Down");
+//		transform.position = new Vector3 (transform.position.x, transform.position.y - 60 * Time.deltaTime, 0);	//this is the right
+//	}
 	public void MoveTo (int inDest)
 	{
 		Dest = inDest;
+		transform.position = new Vector3 (GamePlay.GameBoard.getUnityCoords (Dest, 0) + offX, GamePlay.GameBoard.getUnityCoords (Dest, 1) + offY, 0);	//this is the right
 	}
 }
